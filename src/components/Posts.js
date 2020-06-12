5ce48e8a442078dbdda6f02e8c7c2ab9b4e557c4
danielxurbina <danielur1999@gmail.com>
2020-06-12 15:54:15 -0400

    added likes functionality

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -42,21 +42,45 @@ class Posts extends React.Component {
         .then(data => console.log(data))
     }
 
+    checkLikes = () => {
+        const {id} = this.props.song
+        const filteredLikes = this.props.likes.filter(like => like.song.id === id)
+       
+        if(this.props.currentUser){
+            const currentLike = filteredLikes.filter(like => like.user.id === this.props.currentUser.id)
+            if(currentLike.length > 0){
+                return <h4>Likes: {filteredLikes.length} <button className="likeButton" value={currentLike[0].id} onClick={(event) => this.props.deleteLike(event)}>❤️</button></h4>
+            }
+            else {
+                return <h4>Likes: {filteredLikes.length} <button className="likeButton" value={id} onClick={(event) => this.props.likePost(event)}>❤️</button> </h4>
+            }
+        }
+        else {
+           return <h4>Likes: {filteredLikes.length} </h4>
+        }
+            // if(filteredLikes.includes(id && this.props.likes.id)){
+            //     return <h4>Likes: {filteredLikes.length} <button className="likeButton" value={null} onClick={(event) => this.props.deleteLike(event)}>❤️</button></h4>
+            // }
+            // else{
+            //     return <h4>Likes: {filteredLikes.length} <button className="likeButton" value={id} onClick={(event) => this.props.likePost(event)}>❤️</button> </h4>
+            // }
+    }
+
     render(){
         const {post_image, description, id} = this.props.song
         const {username} = this.props.song.user
         const {userPlaylists, text, comments, likes} = this.props
-        // console.log(this.props.song)
+        const filteredComments = comments.filter(comment => comment.song.id === id)
         return(
             <div>
                 <img src={post_image} alt={username}/>
                 <DropdownButton id="dropdown-basic-button" title="Add Song To Playlist">
                    {userPlaylists.map(playlist => <Dropdown.Item onClick={() => this.handleClick(playlist.id, id)}>{playlist.title}</Dropdown.Item>)}
                 </DropdownButton>
-                <h4>Likes: {likes.map(like => like.song.id === id ? like.length : null)} <button className="likeButton" value={id} onClick={(event) => this.props.likePost(event)}>❤️</button> </h4>
                 <h4>{username} {description}</h4>
+                {this.checkLikes()}
                 <audio ref="audio_tag" src={`http://localhost:3000/${this.state.postSongs}`} controls/>
-                {comments.map(comment => <Comment comment={comment} key={comment.id} songs={this.props.song}/>)}
+                {filteredComments.map(comment => <Comment comment={comment} key={comment.id} songs={this.props.song}/>)}
                 <CommentForm text={text} commentSubmitHandler={this.props.commentSubmitHandler} inputHandler={this.props.inputHandler} songID={id}/>
             </div>
         )
--- src/containers/ProfilePageContainer.js
+++ src/containers/ProfilePageContainer.js
@@ -1,45 +1,67 @@
 import React from 'react'
 import PostCards from '../components/PostCards'
+import axios from 'axios'
 const usersURL = "http://localhost:3000/users"
 
 class ProfilePageContainer extends React.Component {
-    state = {isClicked: false, id: null, username: "", password: "", bio: "", background_image_url: ""}
+    state = {
+        isClicked: false, 
+        id: null, 
+        name: "", 
+        username: "", 
+        password: "", 
+        bio: "", 
+        background_image_url: "", 
+        image: {}
+    }
 
     componentDidMount(){
         fetch(`${usersURL}/${this.props.currentUser.id}`)
         .then(response => response.json())
         .then(data => this.setState({
-            id: data.id,
-            username: data.username,
-            password: data.password,
-            bio: data.bio,
-            background_image_url: data.background_image_url
-        }))
+                id: data.id,
+                name: data.name,
+                username: data.username,
+                password: data.password,
+                bio: data.bio,
+                background_image_url: data.background_image_url
+            })
+        )
     }
 
     toggleForm = () => {this.setState({isClicked: !this.state.isClicked})}
 
-    handleFormChange = (event) => {this.setState({[event.target.name]: event.target.value})}
+    handleFormChange = (event) => {
+        if(event.target.name === 'image'){
+            this.setState({[event.target.name]: event.target.files[0]})
+        }
+        else {
+            this.setState({[event.target.name]: event.target.value})
+        }
+    }
 
     renderEditForm = () => {
-        const {username, password, bio, background_image_url} = this.state
+        const {name, username, password, bio, background_image_url} = this.state
         return(
-            <form>
-                <input type="text" name="username" placeholder="Username" value={username}/>
-                <input type="text" name="password" placeholder="Password" value={password}/>
-                <input type="text" name="bio" placeholder="Bio" value={bio}/>
-                <input type="text" name="background_image_url" placeholder="Background Image" value={background_image_url}/>
-                <input type="file" name="image" placeholder="Image"/>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="name" value={name} placeholder="Name" onChange={(event) => this.handleFormChange(event)}/>
+                <input type="text" name="username" value={username} placeholder="Username" onChange={(event) => this.handleFormChange(event)}/>
+                <input type="text" name="password" value={password} placeholder="Password" onChange={(event) => this.handleFormChange(event)}/>
+                <input type="text" name="bio" value={bio} placeholder="Bio" onChange={(event) => this.handleFormChange(event)}/>
+                <input type="text" name="background_image_url" value={background_image_url} placeholder="Background Image" onChange={(event) => this.handleFormChange(event)}/>
+                <input type="file" name="image"/>
+                <button type="submit">Submit Changes</button>
             </form>
         )
     }
 
-    handleSubmit = () => {
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const formProfileData = new FormData(event.target)
 
+        axios.patch(`${usersURL}/${this.state.id}`, formProfileData)
+        .then(response => this.props.updateUser(response.data))
     }
-
-
-
 
     render(){
         const {userSongs, currentUser, currentUserImage} = this.props
@@ -48,7 +70,8 @@ class ProfilePageContainer extends React.Component {
             <div>
                 <h1>{name}</h1>
                 <p>{bio}</p>
-                <img className="profile-image" src={`http://localhost:3000/${currentUserImage}`} alt={this.props.currentUser.name}/><br></br>
+                <img className="profile-image" src={`http://localhost:3000/${currentUserImage}`} alt={this.props.currentUser.name}/>
+                <br></br>
                 {this.state.isClicked ? this.renderEditForm() : ''}
                 <button onClick={this.toggleForm}>Edit Profile</button>
                 {userSongs.map(userSong => <PostCards userSong={userSong} key={userSong.id} currentUser={currentUser}/>)}
